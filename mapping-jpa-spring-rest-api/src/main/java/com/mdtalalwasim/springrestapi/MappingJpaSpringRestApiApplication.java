995742aba83c3b1408b67c7afcdfda959ff461ab
Md. Talal Wasim <md.talalwasim@gmail.com>
2023-06-07 23:04:43 +0600

    Project Setup - Initial Commit.

---
+++ mapping-jpa-spring-rest-api/src/main/java/com/mdtalalwasim/springrestapi/MappingJpaSpringRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.mdtalalwasim.springrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MappingJpaSpringRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MappingJpaSpringRestApiApplication.class, args);
+	}
+
+}
---
+++ mapping-jpa-spring-rest-api/src/test/java/com/mdtalalwasim/springrestapi/MappingJpaSpringRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdtalalwasim.springrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MappingJpaSpringRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
